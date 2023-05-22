727eac10c247fa9f6619d1eb119cd0b4ac11c0f3
vamastah <szymorawski@gmail.com>
2023-05-22 21:55:57 +0200

    fix makefile

--- common.mk
+++ common.mk
@@ -10,26 +10,25 @@ DATADIR = datadir
 LANTXT = $(wildcard language/*.txt)
 LANFILES = $(patsubst language/%.txt,$(DATADIR)/%.LAN, $(LANTXT))
 
-DATAEXT = DAT DDD JPG LAN MUS OGG PCX SRC SRF TXT
-DATADEP = $(foreach ext,$(DATAEXT),$(wildcard $(DATADIR)/*.$(ext))) $(LANFILES)
-
 SCRIPTSRC = $(wildcard $(DATADIR)/*.SRC)
 SCRIPTRUN = $(SCRIPTSRC:.SRC=.RUN)
 
+DATAEXT = DAT DDD JPG MUS OGG PCX SRC SRF TXT
+DATADEP = $(foreach ext,$(DATAEXT),$(wildcard $(DATADIR)/*.$(ext))) $(LANFILES) $(SCRIPTRUN)
+
 data: $(DATAFILE)
 
-# resource.res built by mingw only
 clean:
-	rm -rf $(TARGET) $(DATAFILE) $(LANFILES) $(SCRIPTRUN) resource.res
+	rm -rf $(TARGET) $(TARGET).exe $(DATAFILE) $(LANFILES) $(SCRIPTRUN) resource.res
 
 # TBD: ssc should compile only things that changed since last build
-$(DATAFILE): $(SCRIPTRUN) $(DATADEP) | $(DATADIR) $(SSC) $(SDP)
+$(DATAFILE): $(DATADEP) | $(SDP)
 	$(SDP) -p -n -i $(DATADIR) -o $(DATAFILE)
 
-$(DATADIR)/%.LAN: language/%.txt
+$(DATADIR)/%.LAN: language/%.txt | $(SLC)
 	$(SLC) --to-lan $< -o $@
 
-$(SCRIPTRUN): $(SCRIPTSRC)
+$(SCRIPTRUN): $(SCRIPTSRC) | $(SSC)
 	$(SSC) -c -i $(DATADIR)
 
 $(SDP): | sdp
